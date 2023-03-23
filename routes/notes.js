f69226a835966f0ca7619e6698cdf01cce37a82e
Kelvin Shiu <orangswork@gmail.com>
2023-03-24 02:30:54 +0800

    initial

--- routes/notes.js
+++ routes/notes.js
@@ -162,7 +162,7 @@ router.post('/rsignin', express.urlencoded({ extended: true }), async (req, res)
 			else{
 				if( pwd == docs[0]["password"]){
 					var milliseconds = 60 * 30000;
-					res.cookie('managerId', docs[0]["_id"], { maxAge: milliseconds});
+					res.header('Set-Cookie', 'managerId='+docs[0]["_id"] +'; SameSite=None; Secure; maxAge: '+milliseconds);
 					//req.session.userId = ''+docs[0]["_id"];
 					//req.session.name = docs[0]['name'];	
 					res.json("logined");
