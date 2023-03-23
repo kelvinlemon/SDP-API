e8084ad9367b62c006e65e4fe7645b6672e11f7f
Kelvin Shiu <orangswork@gmail.com>
2023-03-24 01:14:45 +0800

    initial

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var app = express();
 };*/
 
 var corsOptions = {
-    origin: 'http://219.78.60.145',
+    origin: ['http://219.78.60.145','http://127.0.0.1:3000', 'http://localhost:3000'],
     credentials: true
 };
 
