db7b693c7a23089922c4bbf8513d0acaf4eaceba
Kelvin Shiu <orangswork@gmail.com>
2023-03-31 00:40:15 +0800

    initial

--- routes/notes.js
+++ routes/notes.js
@@ -248,7 +248,7 @@ router.get('/enable/:table', async (req, res) => {
 				if (docs[0]["status"] != "1" && docs[0] != ""){
 					var random6Number = generateRandomNumberString();
 					tableinfo.update({table:table},{$set:{orderedFood:"", time:formatTime(), status:"1", sessionCode: random6Number}}).then(()=>{
-						send = [{"URL":'/toorder/'+table+'/'+random6Number}];
+						send = [{"URL":'/toorder', "data":{"table":table,"session":random6Number}}];
 						res.json(send);
 					}).catch((error)=>{
 						res.json(error);
