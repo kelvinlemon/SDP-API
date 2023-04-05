1535f687a23d14b8a8f0541d1e6102cf9af549ce
Kelvin Shiu <orangswork@gmail.com>
2023-04-06 03:15:47 +0800

    initial

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 };*/
 
 var corsOptions = {
-    origin: ['http://219.78.60.145','http://127.0.0.1:3000', 'http://localhost:3000','http://127.0.0.1:3001', 'http://localhost:3001'],
+    origin: ['https://restaurant-manage.herokuapp.com','http://127.0.0.1:3000', 'http://localhost:3000','http://127.0.0.1:3001', 'http://localhost:3001'],
     credentials: true
 };
 
