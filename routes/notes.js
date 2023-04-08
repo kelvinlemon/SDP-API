e98f79e1476f1c3753a955c16a18a77f1b1159a5
Kelvin Shiu <orangswork@gmail.com>
2023-04-08 19:40:49 +0800

    initial

--- routes/notes.js
+++ routes/notes.js
@@ -1202,17 +1202,24 @@ try {
 					for (const [key, value] of Object.entries(obj)) {
 						str += `${key}: ${value}, `;
 					}
-				}
-				const response = await openai.createChatCompletion({
-					model: "gpt-3.5-turbo",
-					messages: [{ "role": "user", "content": `Act as a AI health assistant, analyze the data for me and give health advice in 50 words - 1. Grains, 2. Vegetables, 3. Fruits, 4. "Meat, fish, egg and alternatives", 5. "milk and alternatives", 6. "food and drinks with high Fat/oil, salt and sugar": ${str}`}],
-					max_tokens:150,
-				})
-			
-				return res.status(200).json({
-					success: true,
-					data: response.data.choices[0].message.content,
-				});
+				} 
+				try{
+					const response = await openai.createChatCompletion({
+						model: "gpt-3.5-turbo",
+						messages: [{ "role": "user", "content": `Act as a AI health assistant, analyze the data for me and give health advice in 50 words - 1. Grains, 2. Vegetables, 3. Fruits, 4. "Meat, fish, egg and alternatives", 5. "milk and alternatives", 6. "food and drinks with high Fat/oil, salt and sugar": ${str}`}],
+						max_tokens:150,
+					})
+				
+					return res.status(200).json({
+						success: true,
+						data: response.data.choices[0].message.content,
+					});
+				}catch (error){
+					return res.status(200).json({
+						success: true,
+						data: error,
+					});
+				};
 			}else{
 				return res.status(200).json({
 					success: true,
